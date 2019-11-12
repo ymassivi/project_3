ea3abdb9b0d38309760f69e8e3ca1ed112691ec1
ymassivi <54606260+ymassivi@users.noreply.github.com>
2019-11-12 08:23:08 -0600

    Update mymap.js

--- mymap.js
+++ mymap.js
@@ -9,12 +9,4 @@ function initMap() {
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
-    </script>
-    <!--Load the API from the specified URL
-    * The async attribute allows the browser to render the page while the API loads
-    * The key parameter will contain your own API key (which is not needed for this tutorial)
-    * The callback parameter executes the initMap() function
-    -->
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWJwWNbCAS-WJmRqQVj9O6A_CItmP1uvU&callback=initMap">
-    </script>
+
