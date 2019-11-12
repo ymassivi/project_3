13969299593d6fb26329eff455f67c310578cb0f
ymassivi <54606260+ymassivi@users.noreply.github.com>
2019-11-12 01:08:10 -0600

    Delete map.js

--- map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-  // The location of willis Tower
-  var willis = {lat: 41.8789, lng: 87.6359};
-  // The map, centered at willis
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 8, center: willis});
-  // The marker, positioned at willis Tower
-  var marker = new google.maps.Marker({position: willis, map: map});
-}
