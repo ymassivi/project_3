44d9fa69f28fcfa4171ba10cc6922cc35cb37858
ymassivi <54606260+ymassivi@users.noreply.github.com>
2019-11-12 23:37:46 -0600

    Update map1.js

--- map1.js
+++ map1.js
@@ -1,6 +1,6 @@
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
+          zoom: 2,
           center: {lat:4.896318, lng: -36.129691},
           mapTypeId: 'terrain'
         });
